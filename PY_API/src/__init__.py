03d5e66e0a8cd8875d0f7266837a8a6615abd410
franck.ledoux <franck.ledoux@live.fr>
2024-01-28 22:41:23 +0100

    update for python integration

---
+++ PY_API/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+PY_API
+
+The toy example Python API.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'franck ledoux'
+__credits__ = 'univ evry'
---
+++ PY_API/tst/test_A.py
@@ -0,0 +1,7 @@
+import sys
+from PY_API import *
+
+
+def test_A_f():
+    a = A()
+    assert a.f(3) == 1
